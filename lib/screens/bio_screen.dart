9d83512422178c6e90f3fb5943f86d5735ad2026
Khaled <khaled.shbair12@gmail.com>
2023-07-01 12:11:53 +0300

    7/1/2023

--- lib/screens/bio_screen.dart
+++ lib/screens/bio_screen.dart
@@ -1,9 +1,9 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter/material.dart';
+import '../widgets/my_icon.dart';
+import '../units/helper.dart';
 import '../Functions.dart';
-import '../helper.dart';
-import '../widgets/my_icon_new.dart';
 
 class BioScreen extends StatefulWidget {
   const BioScreen({super.key});
@@ -24,7 +24,7 @@ class _BioScreenState extends State<BioScreen> with Helper {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             SizedBox(
-              height: 250,
+              height: MediaQuery.of(context).size.height / 3.5,
               child: Stack(
                 alignment: AlignmentDirectional.bottomCenter,
                 clipBehavior: Clip.antiAlias,
@@ -33,8 +33,8 @@ class _BioScreenState extends State<BioScreen> with Helper {
                     alignment: AlignmentDirectional.topCenter,
                     child: Image.asset(
                       'assets/images/cover_image.jpg',
-                      height: 200,
-                      width: MediaQuery.sizeOf(context).width,
+                      height: MediaQuery.of(context).size.height / 4.5,
+                      width: MediaQuery.of(context).size.width,
                       fit: BoxFit.cover,
                     ),
                   ),
@@ -50,7 +50,7 @@ class _BioScreenState extends State<BioScreen> with Helper {
                 ],
               ),
             ),
-            const SizedBox(height: 10),
+            SizedBox(height: MediaQuery.of(context).size.height / 100),
             const Text(
               'Khaled Shbair',
               style: TextStyle(
@@ -69,93 +69,100 @@ class _BioScreenState extends State<BioScreen> with Helper {
                 fontSize: 18,
               ),
             ),
-            const SizedBox(height: 15),
-            MyIconNew(
-              colors: [
-                Colors.green.shade800,
-                Colors.green.shade500,
-              ],
-              icon: FontAwesomeIcons.phone,
-              text: 'Call me',
-              onTap: () => _myFunctions.makePhoneCall('0599724037', context),
-            ),
-            MyIconNew(
-              colors: [
-                Colors.blue.shade800,
-                Colors.blue.shade500,
-              ],
-              icon: FontAwesomeIcons.commentSms,
-              text: 'Sent me sms',
-              size: 30,
-              onTap: () => _myFunctions.sendSMS('0599724037', context),
-            ),
-            MyIconNew(
-              colors: const [
-                Color(0XFF229ED9),
-                Color(0XFF2AABEE),
-              ],
-              icon: FontAwesomeIcons.telegram,
-              size: 30,
-              text: 'Telegram',
-              onTap: () => _myFunctions.openTelegramChat('0599724037', context),
-            ),
-            MyIconNew(
-              colors: const [
-                Color(0XFF14D244),
-                Color(0XFF25D366),
-              ],
-              icon: FontAwesomeIcons.whatsapp,
-              size: 30,
-              text: 'WhatsApp',
-              onTap: () => _myFunctions.openWhatsAppChat(
-                  '0599724037', 'مرحبا خالد', context),
-            ),
-            MyIconNew(
-              colors: const [
-                Color(0XFF4267B2),
-                Color(0XFF3156B0),
-              ],
-              icon: FontAwesomeIcons.facebook,
-              text: 'Facebook',
-              size: 30,
-              onTap: () => _myFunctions.openFacebookProfile(context),
-            ),
-            MyIconNew(
-              colors: const [
-                Color(0XFF405DE6),
-                Color(0XFF5B51D8),
-                Color(0XFF833AB4),
-                Color(0XFFC13584),
-                Color(0XFFE1306C),
-                Color(0XFFFD1D1D),
-                Color(0XFFF56040),
-                Color(0XFFF77737),
-                Color(0XFFFCAF45),
-                Color(0XFFFFDC80),
-              ],
-              icon: FontAwesomeIcons.instagram,
-              text: 'Instagram',
-              size: 30,
-              onTap: () => _myFunctions.openInstagramProfile(context),
-            ),
-            MyIconNew(
-              colors: const [
-                Color(0XFF4285F4),
-                Color(0XFFDB4437),
-                Color(0XFFF4B400),
-                Color(0XFF0F9D58),
-              ],
-              leading: CircleAvatar(
-                backgroundColor: Colors.white,
-                radius: 16,
-                child: SvgPicture.asset(
-                  'assets/icons/gmail.svg',
-                  height: 26,
-                  fit: BoxFit.cover,
+            SizedBox(height: MediaQuery.of(context).size.height / 60),
+            ListView(
+              shrinkWrap: true,
+              children: [
+                MyIcon(
+                  colors: [
+                    Colors.green.shade800,
+                    Colors.green.shade500,
+                  ],
+                  icon: FontAwesomeIcons.phone,
+                  text: 'Call me',
+                  onTap: () =>
+                      _myFunctions.makePhoneCall('0599724037', context),
                 ),
-              ),
-              text: 'Gmail',
-              onTap: () => _myFunctions.sendMessageGmail(context),
+                MyIcon(
+                  colors: [
+                    Colors.blue.shade800,
+                    Colors.blue.shade500,
+                  ],
+                  icon: FontAwesomeIcons.commentSms,
+                  text: 'Sent me sms',
+                  size: 30,
+                  onTap: () => _myFunctions.sendSMS('0599724037', context),
+                ),
+                MyIcon(
+                  colors: const [
+                    Color(0XFF229ED9),
+                    Color(0XFF2AABEE),
+                  ],
+                  icon: FontAwesomeIcons.telegram,
+                  size: 30,
+                  text: 'Telegram',
+                  onTap: () =>
+                      _myFunctions.openTelegramChat('0599724037', context),
+                ),
+                MyIcon(
+                  colors: const [
+                    Color(0XFF14D244),
+                    Color(0XFF25D366),
+                  ],
+                  icon: FontAwesomeIcons.whatsapp,
+                  size: 30,
+                  text: 'WhatsApp',
+                  onTap: () => _myFunctions.openWhatsAppChat(
+                      '0599724037', 'مرحبا خالد', context),
+                ),
+                MyIcon(
+                  colors: const [
+                    Color(0XFF4267B2),
+                    Color(0XFF3156B0),
+                  ],
+                  icon: FontAwesomeIcons.facebook,
+                  text: 'Facebook',
+                  size: 30,
+                  onTap: () => _myFunctions.openFacebookProfile(context),
+                ),
+                MyIcon(
+                  colors: const [
+                    Color(0XFF405DE6),
+                    Color(0XFF5B51D8),
+                    Color(0XFF833AB4),
+                    Color(0XFFC13584),
+                    Color(0XFFE1306C),
+                    Color(0XFFFD1D1D),
+                    Color(0XFFF56040),
+                    Color(0XFFF77737),
+                    Color(0XFFFCAF45),
+                    Color(0XFFFFDC80),
+                  ],
+                  icon: FontAwesomeIcons.instagram,
+                  text: 'Instagram',
+                  size: 30,
+                  onTap: () => _myFunctions.openInstagramProfile(context),
+                ),
+                MyIcon(
+                  colors: const [
+                    Color(0XFF4285F4),
+                    Color(0XFFDB4437),
+                    Color(0XFFF4B400),
+                    Color(0XFF0F9D58),
+                  ],
+                  leading: CircleAvatar(
+                    backgroundColor: Colors.white,
+                    radius: 16,
+                    child: SvgPicture.asset(
+                      'assets/icons/gmail.svg',
+                      height: 26,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  text: 'Gmail',
+                  onTap: () => _myFunctions.sendMessageGmail(context),
+                ),
+              ],
             ),
             Container(
               margin: const EdgeInsetsDirectional.only(bottom: 10, top: 10),
